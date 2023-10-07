9d2f11f779536776582de9fd39673008cbd0920f
Iveth Plata <karen.plata2223@gmail.com>
2023-10-07 15:37:38 -0600

    crear DB

--- js/classes/App.js
+++ js/classes/App.js
@@ -16,6 +16,17 @@ class App {
     }
 
     initApp() {
+        // Se ejecuta cuando cargue el documento
+        window.onload = () => {
+            // Ejecutamos los eventos
+            this.eventListeners();
+
+            // Crear DB
+            this.crearDB();
+        }
+    }
+
+    eventListeners() {
         // Inputs
         mascotaInput.addEventListener('input', datosCita);
         propietarioInput.addEventListener('input', datosCita);
@@ -26,6 +37,43 @@ class App {
 
         // Formulario
         formulario.addEventListener('submit', nuevaCita);
+    }
+
+    crearDB() {
+        // Crear DB en version 1.0
+        const crearDB = window.indexedDB.open('citas', 1);
+
+        // Si hay error
+        crearDB.onerror = function() {
+            console.log('Error al crear DB');
+        }
+
+        // Si se creo bien
+        crearDB.onsuccess = function() {
+            console.log('DB Creada');
+        }
+
+        // Configuracion de DB
+        crearDB.onupgradeneeded = function(e) {
+            const db = e.target.result;
+
+            // Crear tablae y id
+            const objectStore = db.createObjectStore('citas', {
+                keyPath: 'id',
+                autoIncrement: true
+            });
+
+            // Definir todas las columnas
+            objectStore.createIndex('mascota', 'mascota', {unique: false});
+            objectStore.createIndex('propietario', 'propietario', {unique: false});
+            objectStore.createIndex('telefono', 'telefono', {unique: false});
+            objectStore.createIndex('fecha', 'fecha', {unique: false});
+            objectStore.createIndex('hora', 'hora', {unique: false});
+            objectStore.createIndex('sintomas', 'sintomas', {unique: false});
+            objectStore.createIndex('id', 'id', {unique: true});
+
+            console.log('Columnas creadas');
+        }
     }
 
 }
